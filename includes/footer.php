32201301d4a771fb9e0b90e76051864495540460
Prashant Choudhary <105124007+Prashant0064@users.noreply.github.com>
2024-06-22 11:25:47 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2024</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
